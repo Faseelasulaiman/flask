0c9019f523a8725e6e18ba15aed8a5cf310bae06
Faseelasulaiman <faseelasulaiman127@gmail.com>
2026-09-17 11:20:45 +0530

    third commit

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
 def about():
     return "This is my flask application"
 
+@app.route("/contact")
+def contact():
+    return "This is contact page of my flask application"
+
+
 if __name__== "__main__":
     app.run(debug=True)
